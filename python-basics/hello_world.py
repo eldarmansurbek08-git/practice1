e10eaac032ca8a643cec22f6a416a80791458243
Eldar Mansurbek <eldar.mansurbek.08@gmail.com>
2026-02-07 14:33:19 +0500

    Add hello_world.py with 5 examples

--- python-basics/hello_world.py
+++ python-basics/hello_world.py
@@ -1,1 +1,14 @@
-print("Hello Git!")
+# Example 1
+print("Hello, World!")
+
+# Example 2
+print("Hello from Python")
+
+# Example 3
+print("My first Python program")
+
+# Example 4
+print("Practice 1")
+
+# Example 5
+print("GitHub + Python")
