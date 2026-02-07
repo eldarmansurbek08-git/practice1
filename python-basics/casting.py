6b6e41095b175886e08db69dc45dbd8d52b45f9f
Eldar Mansurbek <eldar.mansurbek.08@gmail.com>
2026-02-07 14:40:40 +0500

    Add casting.py with 5 examples

--- python-basics/casting.py
+++ python-basics/casting.py
@@ -1,4 +1,19 @@
-a = 10
-b = 3.14
-c = str(a)
-print(a, b, c)
+# Example 1
+x = int(3.7)
+print(x)
+
+# Example 2
+y = float(5)
+print(y)
+
+# Example 3
+z = str(100)
+print(z)
+
+# Example 4
+a = int("25")
+print(a)
+
+# Example 5
+b = float("3.14")
+print(b)
