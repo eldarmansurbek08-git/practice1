45188e8d127dd9f28bd34a498c718021bc0fe34c
Eldar Mansurbek <eldar.mansurbek.08@gmail.com>
2026-02-07 16:51:43 +0500

    Add strings.py example

--- python-basics/strings.py
+++ python-basics/strings.py
@@ -1,4 +1,19 @@
-s = "Python"
-print(s[0:3])
-print(s.upper())
-print(f"Length: {len(s)}")
+# Example 1
+text = "Hello"
+print(text)
+
+# Example 2
+text = "Python"
+print(text[0])
+
+# Example 3
+text = "Python"
+print(len(text))
+
+# Example 4
+text = "Hello World"
+print(text.upper())
+
+# Example 5
+text = "Hello World"
+print(text.lower())
