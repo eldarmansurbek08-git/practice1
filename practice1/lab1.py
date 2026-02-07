64186a7c7579cef472b7d569cfecb689c0ba5fd5
Eldar Mansurbek <eldar.mansurbek.08@gmail.com>
2026-02-07 17:39:22 +0500

    practice 1

---
+++ practice1/lab1.py
@@ -0,0 +1,112 @@
+# lab - 1
+
+# 101
+# s = input()
+# print("Hello,", s + "!")
+
+# 102
+# a = input()
+# b = input()
+# print(a +  "***" + b)
+
+# 103
+# a = input()
+# if a.isdigit():
+#     print("int")
+# else: 
+#     print("str")
+
+# 104
+# a = input()
+# b = input()
+# print(int(a)+int(b))
+
+# 105
+# a = int(input())
+# b = int(input())
+# c = a // b
+# d = a / b
+# print(c)
+# print(d)
+
+# 106
+# a = int(input())
+# b = int(input())
+# print(a**b)
+
+# 107
+# a = int(input())
+# b = int(input())
+# print(a%b)
+
+# 108
+# a = input()
+# b = int(input())
+# print(a*b)
+
+# 109
+# a = input()
+# print(len(a))
+
+# 110
+# a = input()
+# print(a.upper())
+# print(a.lower())
+
+# 111
+# a = input()
+# print(a[0], a[-1])
+
+# 112
+# a = input()
+# print(a[2:5])
+
+# 113
+# a = input()
+# print(a[::-1])
+
+# 114
+# a = input()
+# b = int(input())
+# print("Hello,", a + ". You are", b, "years old.")
+
+# 115
+# long = input()
+# short = input()
+# if short in long:
+#     print(bool(-1))
+# else:
+#     print(bool(0))
+
+# 116
+# a = input()
+# b = input()
+# print(a+b)
+
+# 117
+# a = input()
+# b = input()
+# print(b, a)
+
+# 118
+# a = int(input())
+# if a % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# 119
+# a = input()
+# b = input()
+# c = input()
+# print(a.replace(b,c))
+
+# 120
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(a)
+# elif a < b:
+#     print(b)
+# else:
+#     print("equal")
