32767c2404dd30b770ddae77b9c7562ca61f6f71
Eldar Mansurbek <eldar.mansurbek.08@gmail.com>
2026-02-11 22:44:47 +0500

    Add Boolean examples

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,8 @@
+# 2. Booleans as Comparison Results
+x = 10
+y = 20
+print(x == y)  # False
+print(x != y)  # True
+print(x > y)   # False
+print(x < y)   # True
+print(x >= 10) # True
---
+++ practice2/boolean/boolean_operators.py
@@ -0,0 +1,9 @@
+# 3. Boolean Operators
+a = True
+b = False
+
+print(a and b)  # False
+print(a or b)   # True
+print(not a)    # False
+print(not b)    # True
+print(a and not b) # True
